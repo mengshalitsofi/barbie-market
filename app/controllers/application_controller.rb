6e5032093b127c18748d9c5e5611aee2d27af7d6
mengshalitsofi <mengshalitsofi@gmail.com>
2021-04-01 13:45:02 -0700

    adding basic rails files

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+
+    helper_method(:current_user)
+  
+    def current_user
+      @current_user = User.find_by(id: session[:user_id])
+    end
+  
+    def require_login
+      redirect_to '/login' if !current_user
+    end
+  
+  end  
---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  resources :users, except: [:show, :index, :new, :create, :destroy, :patch, :put] do
+    resources(:products, except: [:show, :edit, :update, :destroy, :put])
+  end
+
+  resources(:products, except: :put) do
+    resources :listings, only: [:new, :create]
+  end
+
+  resources :listings, only: [:new, :create, :index]
+  # get '/items', to: "items#index"
+  # get '/items/:id', to: "items#show"
+  get '/signup', to: "users#new", as: "signup"
+  post '/signup', to: "users#create"
+  get '/login', to: "sessions#new", as: "login"
+  post '/login', to: "sessions#create"
+  post '/logout', to: "sessions#destroy"
+  get "/auth/facebook/callback", to: "sessions#create_with_fb"
+  # resources(:items)
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
