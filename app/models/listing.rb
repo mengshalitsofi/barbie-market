d166eba771a67e35fd7964b8906b1fc02bc5d94c
mengshalitsofi <mengshalitsofi@gmail.com>
2021-04-09 08:49:02 -0700

    remove listings_attributes=

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,6 +3,7 @@ class Listing < ApplicationRecord
     belongs_to :user
     validates_presence_of :quantity
 
+
     scope(:quantity_search, ->(quantity) { self.where("quantity >= ?", quantity) }) 
 end
   
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,16 +4,18 @@ class Product < ApplicationRecord
     validates :price, presence: true
     has_many :listings
     has_many :users, through: :listings
-  
-    def listings_attributes=(attrs)
-      attrs.values.each do |hash|
-        if hash[:id]
-          l = Listing.find_by(id: hash[:id])
-          l.update(hash)
-        else
-          self.listings.build(hash)
-        end
-      end
-    end
+    
+    accepts_nested_attributes_for :listings
+
+    # def listings_attributes=(attrs)
+    #   attrs.values.each do |hash|
+    #     if hash[:id]
+    #       l = Listing.find_by(id: hash[:id])
+    #       l.update(hash)
+    #     else
+    #       self.listings.build(hash)
+    #     end
+    #   end
+    # end
   end
   
